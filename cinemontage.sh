231b745608dd524e89810b584018550d875d63ee
1enn0 <lehafb15@gmail.com>
2015-03-29 15:26:10 +0200

    add tiny and cleanup option

--- cinemontage.sh
+++ cinemontage.sh
@@ -4,9 +4,27 @@
 
 #! /bin/bash -
 
+# set initial values for flags
+TINY_FLAG=0
+CLEANUP_FLAG=0
+
+# read the options
+ARGS=`getopt -o ct --long cleanup,tiny -n 'cinemontage.sh' -- "$@"`
+eval set -- "$ARGS"
+
+# extract options set flags
+while true ; do
+    case "$1" in
+        -c|--cleanup) CLEANUP_FLAG=1 && echo "CLEANUP_FLAG set" ; shift ;;
+        -t|--tiny) TINY_FLAG=1 && echo "TINY_FLAG set" ; shift ;;
+        --) shift ; break ;;
+        *) echo "Internal error!" ; exit 1 ;;
+    esac
+done
+
 INPUTFILE=$1
 BASENAME=${INPUTFILE%.*}
-FILENAME=$BASENAME.jpg
+
 
 TMPDIR=./tmp
 if [ ! -d $TMPDIR ]; then
@@ -31,18 +49,33 @@ else
 	exit 1
 fi
 
+
+if [[ $TINY_FLAG == 1 ]]; then
+	H_SIZE=16
+	V_SIZE=9
+	H_SPACE=0
+	V_SPACE=2
+	FILENAME=`printf "%s_tiny.jpg" $BASENAME`
+else
+	H_SIZE=160
+	V_SIZE=90
+	H_SPACE=0
+	V_SPACE=22
+	FILENAME=$BASENAME.jpg
+fi
+
 printf "\nCreating thumbnails...\n"
 # take one image every second and resize to 160x90 px
 avconv -i $INPUTFILE -s 160x90 -vsync 1 -r 1 -an -y $TMPDIR/'%04d.jpg'
 
-printf "\nStitching montage [%s images per row]...\n" $FPS
-# create the montage with 25px vertical spacing, tiling $FPS pics horizontally
-montage -background black -fill white $TMPDIR/*.jpg -tile `echo $FPS`x -geometry 160x90\>+0+25 -title $BASENAME $FILENAME
+printf "Stitching montage [%s images per row]...\n" $FPS
+# create the montage
+montage -background black $TMPDIR/*.jpg -tile `echo $FPS`x -geometry `echo $H_SIZE`x`echo $V_SIZE`\>+`echo $H_SPACE`+`echo $V_SPACE` $FILENAME
 
-
-
-if [[ $2 == "clean" ]]; then
+if [[ $CLEANUP_FLAG == 1 ]]; then
+	printf "Cleaning up...\n"
 	rm -r $TMPDIR
 fi
 
-printf "\nAll done.\n"
+
+printf "All done.\n"
